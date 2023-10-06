a3470ce1677ba794e7c101bb7968af9d72a4f453
Arghya-Bandyopadhyay30 <arghyaarya3003@gmail.com>
2023-10-06 19:00:26 +0530

    Test Cases Running Successfully

--- src/test/powerpackage/powertest.java
+++ src/test/powerpackage/powertest.java
@@ -1,24 +1,25 @@
 package powerpackage;
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
 public class powertest {
     @Test
     public void one_raised_to_one_is_onew() {
-        assert powerfinder.OF(1,1) == 1;
+        assertEquals(powerfinder.OF(1,1),1);
     }
 
     @Test
     public void twoRaised_to_one_is_2() {
-        assert powerfinder.OF(2,1) == 2;
+        assertEquals(powerfinder.OF(2,1),2);
     }
 
     @Test
     public void two_power_2_is_4() {
-        assert powerfinder.OF(2, 2) == 4;
+        assertEquals(powerfinder.OF(2,2),4);
     }
 
     @Test
     public void power_of_2_and_3_Is_SIX() {
-        assert powerfinder.OF(3, 2) == 3*3;
+        assertEquals(powerfinder.OF(3,2),9);
     }
 }
